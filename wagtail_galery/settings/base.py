50f49e527b044095530b3b0dcc4c37acde115233
dolidodzik <teethtard321@gmail.com>
2019-05-07 19:43:32 +0200

    Added basic bootstrap template

--- wagtail_galery/settings/base.py
+++ wagtail_galery/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wagtail.contrib.settings',
     'gallery',
 ]
 
@@ -79,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ],
         },
     },
